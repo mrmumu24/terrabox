ee7fa2bd86ec14c33616d28b7390132a689a75c5
mrmumu24 <mrmumu24@gmail.com>
2020-12-17 19:39:31 +0100

    dont assign EIP to instance on launch

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -9,10 +9,9 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "subnets" {
   count = length(var.availability_zones)
 
-  vpc_id                  = var.vpc_id
-  cidr_block              = var.subnets[count.index]
-  availability_zone       = var.availability_zones[count.index]
-  map_public_ip_on_launch = true
+  vpc_id            = var.vpc_id
+  cidr_block        = var.subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
 
   tags = {
     Name = join("_", [
